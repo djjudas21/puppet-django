85ccc280d338b2fcc16ac1d369d37e82ce5c46ea
Jonathan Gazeley <jonathan.gazeley@bristol.ac.uk>
2015-02-10 11:14:10 +0000

    Very early prototype of django deployment

---
+++ manifests/init.pp
@@ -0,0 +1,19 @@
+# Deploy a Django app from git
+class django (
+  $source,  # Source of app in git
+  $path,    # Destination path on filesystem
+  $url,     # URL that will be used to serve the app
+  $ensure = 'present',
+  $revision = undef, # Revision of the app
+) {
+
+  # Create the directory where the app will be installed
+  file { $path:
+    ensure => directory,
+  }
+
+  # Check out the app from version control
+  vcsrepo { $path:
+    ensure => present,
+  }
+}
